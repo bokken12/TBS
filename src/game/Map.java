ecfc4e6beea3ca74608cb5d3f0e028619ac50d26
CaseyManning <kcgrind@gmail.com>
2017-07-01 20:51:55 -0700

    map and tile work

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
@@ -10,7 +11,17 @@ import javax.swing.JPanel;
 public class Map extends JPanel implements MouseListener {
 
 	Tile[][] map;
+	public final int TILE_SIZE = 64;
 
+	@Override
+	public void paintComponent(Graphics g) {
+		for(int y = 0; y < map.length; y++) {
+			for(int x = 0; x < map[0].length; x++) {
+				g.drawImage(map[y][x].getImage(), x*(TILE_SIZE/2)*3, y*TILE_SIZE/2, this);
+			}
+		}
+	}
+	
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
--- src/game/Tile.java
+++ src/game/Tile.java
@@ -1,7 +1,28 @@
 package game;
 
+import java.awt.Image;
+import java.util.IdentityHashMap;
+
+import javax.swing.ImageIcon;
+
 public class Tile {
 
+	ImageIcon image;
+	
+	static enum Type {
+		TREE("wood"), PLAINS("sheep"), WATER("food"), BOULDERS("stone"), FARM("food");
+		
+		private String resource;
+		
+		Type(String resource){
+			this.resource = resource;
+		}
+	}
+	
+	public Image getImage() {
+		return image.getImage();
+	}
+	
 	public Tile() {
 		
 	}
