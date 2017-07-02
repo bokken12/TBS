e8e73c3688e695859328b57a6140e58229625471
CaseyManning <kcgrind@gmail.com>
2017-07-01 17:48:18 -0700

    added classes

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,5 +1,7 @@
 package game;
 
-public class Game {
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
 
 }
--- src/game/Main.java
+++ src/game/Main.java
@@ -1,9 +1,14 @@
 package game;
 
-/**
- * @author joel
- *
- */
+import javax.swing.JFrame;
+
 public class Main {
 	
+	public static void main(String[] args) {
+		
+	}
+	
+	public Main() {
+		
+	}
 }
