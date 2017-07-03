d2ce8d67f7158b0b29b414a9007b7856204a9450
CaseyManning <kcgrind@gmail.com>
2017-07-03 06:47:12 -0700

    created menu class

--- src/game/Menu.java
+++ src/game/Menu.java
@@ -4,6 +4,8 @@
 package game;
 
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -11,7 +13,7 @@ import javax.swing.JPanel;
  * @author nyyyar
  *
  */
-public class Menu extends JPanel {
+public class Menu extends JPanel implements MouseListener {
 	
 	public Menu() {
 		
@@ -19,6 +21,35 @@ public class Menu extends JPanel {
 	
 	@Override
 	public void paintComponent(Graphics g) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		CardDeck.getInstance().getLayout().show(new Game(), "game");
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
